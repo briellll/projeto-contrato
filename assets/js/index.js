e638267a925acc40236cf6085b33516334387f93
Gabriel <carloscgmorais@gmail.com>
2026-02-10 20:04:10 -0300

    first commit

---
+++ assets/js/index.js
@@ -0,0 +1,31 @@
+document.getElementById('open_btn').addEventListener('click',function(){
+    document.getElementById('sidebar').classList.toggle('open-sidebar')
+})
+
+
+const content = document.getElementById('content');
+const links = document.querySelectorAll('#side_items a');
+
+async function loadPage(page) {
+    const response = await fetch(`/pages/${page}.html`);
+    const html = await response.text();
+    content.innerHTML = html;
+
+    const pageStyle = document.getElementById('page-style');
+    pageStyle.href = `assets/css/${page}.css`;
+
+}
+
+links.forEach(link =>{
+    link.addEventListener('click', e=>{
+        e.preventDefault();
+        const page = link.dataset.page;
+        loadPage(page);
+
+        document.querySelectorAll('.side_item')
+        .forEach(li => li.classList.remove('active'));
+        link.closest('.side_item').classList.add('active');
+        });
+});
+
+loadPage('cadastro');

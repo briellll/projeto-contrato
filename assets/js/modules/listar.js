fbbe7db21b4a166f2cee6f3e72f986a6705b547d
Gabriel <carloscgmorais@gmail.com>
2026-02-14 23:51:03 -0300

    refactor: melhorar animação e exibição de detalhes na tabela; ajustar estrutura do HTML e CSS

--- assets/js/modules/listar.js
+++ assets/js/modules/listar.js
@@ -1,3 +1,19 @@
 export function jsListar(){
-    
+    const buttonList = document.querySelectorAll('.btn-trigger-list')
+
+    buttonList.forEach(button =>{
+        button.addEventListener('click', function(){
+
+            const currentRow = this.closest('tr')
+            const detailRow = currentRow.nextElementSibling
+
+            this.classList.toggle('rotated');
+
+            if (currentRow && detailRow.classList.contains('detail-container')){
+                detailRow.classList.toggle('show')
+            }
+
+        })
+    })
+
 }
